0871332b6bcb95062a421b6726b90b5debd3cf22
Yuri Fernandes <yuriinfo15@gmail.com>
2018-08-23 18:40:16 -0300

    Add files via upload

---
+++ controller/controllerUsuarios.class.php
@@ -0,0 +1,7 @@
+<?php 	
+	include_once("../database/Database.class.php");
+	include_once("../model/modelUsuarios.class.php");
+	class controllerUsuarios{
+		
+	}
+?>
---
+++ model/modelUsuarios.class.php
@@ -0,0 +1,5 @@
+<?php 
+	class modelUsuarios{
+		
+	}
+?>
